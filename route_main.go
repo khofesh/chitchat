fbc049470e66e04f412c1c05843762e6c860adbc
Fahmi Ahmad <fahmiahmad@tutanota.com>
2020-06-30 20:45:05 +0700

    change name

--- route_main.go
+++ route_main.go
@@ -1,31 +1,32 @@
 package main
 
 import (
-  "net/http"
-  "chitchat/data"
+	"chitchat/data"
+	"net/http"
 )
 
 // GET /err?msg=
 // shows the error message page
 func err(writer http.ResponseWriter, request *http.Request) {
-  vals := request.URL.Query()
-  _, err := session(writer, request)
-  if err != nil {
-    generateHTML(writer, vals.Get("msg"), "layout", "public.navbar", "error")
-  } else {
-    generateHTML(writer, vals.Get("msg"), "layout", "private.navbar", "error")
-  }  
+	vals := request.URL.Query()
+	_, err := session(writer, request)
+	if err != nil {
+		generateHTML(writer, vals.Get("msg"), "layout", "public.navbar", "error")
+	} else {
+		generateHTML(writer, vals.Get("msg"), "layout", "private.navbar", "error")
+	}
 }
 
 func index(writer http.ResponseWriter, request *http.Request) {
-  threads, err := data.Threads(); if err != nil {
-    error_message(writer, request, "Cannot get threads")
-  } else {
-    _, err := session(writer, request)
-    if err != nil {
-      generateHTML(writer, threads, "layout", "public.navbar", "index")
-    } else {
-      generateHTML(writer, threads, "layout", "private.navbar", "index")
-    }
-  }
+	threads, err := data.Threads()
+	if err != nil {
+		errorMessage(writer, request, "Cannot get threads")
+	} else {
+		_, err := session(writer, request)
+		if err != nil {
+			generateHTML(writer, threads, "layout", "public.navbar", "index")
+		} else {
+			generateHTML(writer, threads, "layout", "private.navbar", "index")
+		}
+	}
 }
